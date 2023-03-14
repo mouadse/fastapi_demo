71dd6ae23acf3b52d18f9ba4ab53bc8bbf502564
mouadse <mouadse@gmail.com>
2023-03-15 00:11:44 +0100

    Added html file to make the returned results more user-friendly

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/weather")
-def get_weather(request: Request, url: str):
-    # Fetch the HTML content from the provided URL
+@app.get("/weather", response_class=HTMLResponse)
+def get_weather(url: str):
+    # Your code to fetch weather information from the URL goes here
     response = requests.get(url)
     html = response.text
-
-    # Parse the HTML using BeautifulSoup
-    soup = BeautifulSoup(html, "html.parser")
-
-    # Extract the temperature value
-    temperature = soup.find("span", {"class": "temp__value"}).text
-
-    # Extract the weather condition
-    weather_condition = soup.find("div", {"class": "link__condition"}).text
-
-    # Extract the feels_like value
-    feels_like = soup.find("span", {"class": "term__value_with-unit"}).text
-
-    # Render the HTML template with the weather information
-    context = {"temperature": temperature, "weather_condition": weather_condition, "feels_like": feels_like, }
-    return templates.TemplateResponse("weather.html", {"request": request, **context})
+    beautiful_soup = BeautifulSoup(html, "html.parser")
+    temperature = beautiful_soup.find("span", {"class": "temp__value temp__value_with-unit"}).text
+    weather_condition = beautiful_soup.find("div", {"class": "link__condition day-anchor i-bem"}).text
+    feels_like = beautiful_soup.find("span", {"class": "temp__value temp__value_with-unit"}).text
+    return """
+       <html>
+           <head>
+               <title>Some HTML in here</title>
+           </head>
+           <body>
+               <h1>Look ma! HTML!</h1>
+               <p>Here's some weather information: {{ temperature }}</p>
+                <p>Here's some weather information: {{ weather_condition }}</p>
+                <p>Here's some weather information: {{ feels_like }}</p>
+           </body>
+       </html>
+       """
