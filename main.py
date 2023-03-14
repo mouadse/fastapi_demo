9cb6fdf9045f85ebcc681417c8e3ae7a843d2365
mouadse <mouadse@gmail.com>
2023-03-15 00:23:30 +0100

    Added html file to make the returned results more user-friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
@@ -10,7 +10,7 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/weather", response_class=HTMLResponse)
-def get_weather(url: str):
+async def get_weather(request: Request, url: str):
     # Your code to fetch weather information from the URL goes here
     response = requests.get(url)
     html = response.text
@@ -18,16 +18,7 @@ def get_weather(url: str):
     temperature = beautiful_soup.find("span", {"class": "temp__value temp__value_with-unit"}).text
     weather_condition = beautiful_soup.find("div", {"class": "link__condition day-anchor i-bem"}).text
     feels_like = beautiful_soup.find("span", {"class": "temp__value temp__value_with-unit"}).text
-    return """
-       <html>
-           <head>
-               <title>Some HTML in here</title>
-           </head>
-           <body>
-               <h1>Look ma! HTML!</h1>
-               <p>Here's some weather information: {{ temperature }}</p>
-                <p>Here's some weather information: {{ weather_condition }}</p>
-                <p>Here's some weather information: {{ feels_like }}</p>
-           </body>
-       </html>
-       """
+
+    context = {"request": request, "temperature": temperature, "weather_condition": weather_condition,
+               "feels_like": feels_like}
+    return templates.TemplateResponse("weather.html", context=context)
