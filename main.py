b08a300c982875e710ebc3bd76db3e86fe2d72c9
mouadse <mouadse@gmail.com>
2023-03-14 23:44:21 +0100

    Added some links and libraries

--- main.py
+++ main.py
@@ -7,15 +7,12 @@ app = FastAPI()
 
 @app.get("/weather")
 def get_weather(url: str):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    lat = soup.find("span", {"class": "latitude"}).text
-    lon = soup.find("span", {"class": "longitude"}).text
-    api_url = f"https://api.meteum.ai/v2/weather?lat={lat}&lon={lon}"
-    api_response = requests.get(api_url)
-    api_data = api_response.json()
-    temperature = api_data["temperature"]["value"]
-    wind_speed = api_data["wind_speed"]["value"]
-    pressure = api_data["pressure"]["value"]
-    description = api_data["description"]
-    return {"temperature": temperature, "wind_speed": wind_speed, "pressure": pressure, "description": description}
+    # Your code to fetch weather information from the URL goes here
+    response = requests.get(url)
+    html = response.text
+    beautiful_soup = BeautifulSoup(html, "html.parser")
+    temperature = beautiful_soup.find("span", {"class": "temp__value temp__value_with-unit"}).text
+    weather_condition = beautiful_soup.find("span", {"class": "temp__value temp__value_with-unit"}).text
+
+
+    pass
